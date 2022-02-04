ff3e711ebd7e72d19a08c40add3e219cd409b3af
krutik228 <nikkrutik@mail.ru>
2022-02-05 02:34:21 +0700

    deleted tables in html, added tags, updated models.py, views.py, urls.py

--- firstsite/news/urls.py
+++ firstsite/news/urls.py
@@ -8,5 +8,6 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('category/<int:category_id>/', get_category, name='category'),
+    path('news/<int:news_id>/', read_news, name='read_news'),
 ]
 
--- firstsite/news/views.py
+++ firstsite/news/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 from news.models import News, Category
 
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей',
-        'categories': categories,
 
     }
     return render(request, template_name='news/index.html', context=context)
@@ -19,11 +15,18 @@ def index(request):
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
-        'categories': categories,
         'category': category,
+        'title': 'Список новостей',
     }
     return render(request, template_name='news/category.html', context=context)
+
+
+def read_news(request, news_id):
+    news_item = News.objects.get(pk=news_id)
+    context = {
+        'item': news_item,
+    }
+    return render(request, template_name='news/read_news.html', context=context)
