9d661566bd35349cee1f1b021db2f4fc63349c68
krutik228 <nikkrutik@mail.ru>
2022-02-10 15:29:00 +0700

    added cash

--- firstsite/firstsite/settings.py
+++ firstsite/firstsite/settings.py
@@ -211,3 +211,10 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'django_cache'),
+    }
+}
--- firstsite/news/urls.py
+++ firstsite/news/urls.py
@@ -2,6 +2,7 @@
 # from django.conf.urls.static import static
 # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from .views import *
 
@@ -10,6 +11,7 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     # path('', index, name='home'),
+    # path('', cache_page(60)(HomeNews.as_view()), name='home'),  # cashed HomePage
     path('', HomeNews.as_view(), name='home'),
     # path('category/<int:category_id>/', get_category, name='category'),
     path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
